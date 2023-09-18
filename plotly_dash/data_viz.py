51a948b0f554e1a2306c295350f9e63c7a20ef8e
kat <kat@pop-os.localdomain>
2023-09-18 17:43:07 +0100

    Add Plotly Dash functionality for demonstrate multiple regression interactivity.

--- plotly_dash/data_viz.py
+++ plotly_dash/data_viz.py
@@ -9,6 +9,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from math import sqrt
+import statsmodels.api as sm
+import plotly.graph_objects as go
 
 class Histogram:
     """A histogram."""
@@ -155,5 +157,140 @@ class LinearRegression:
         """
         fig = px.scatter(df, x=self.explanatory, y=self.response,
                          trendline='ols')
+
+        return fig
+    
+
+class MultipleRegression:
+    """A multiple regression."""
+    def __init__(self, species: str, first_explanatory: str, 
+                 second_explanatory: str, response: str):
+        """Params:
+               species (str): The user's choice of penguin species.       
+               first_explanatory (str): The user's choice for the first 
+                   explanatory variable.
+               second_explanatory (str): The user's choice for the second 
+                   explanatory variable.
+               response (str): The user's choice for the response variable.
+        """
+        self.species = species
+        self.first_explanatory = first_explanatory
+        self.second_explanatory = second_explanatory
+        self.response = response
+
+    def build_query(self):
+        """Build a SQL query from the user's chosen variables and species.
+        
+           Build a SQL query from the user's chosen variables and species
+           which is then passed to create_dataframe().
+           
+           Returns:
+               `fig` from create_graph().
+        """
+        query = text(f"""SELECT {self.first_explanatory}, 
+                                {self.second_explanatory},
+                                {self.response}
+                         FROM palmerpenguins 
+                         WHERE species 
+                         LIKE {self.species}""")
+
+        return self.create_dataframe(query)
+    
+    def create_dataframe(self, query: sqlalchemy.TextClause()) -> go.Figure():
+        """Create a dataframe by querying the database.
+
+           With the query provided by build_query(), connect to the database
+           and place the results in a dataframe. Finally, pass the (clean) 
+           dataframe to create_graph().
+
+           Params:
+               query (`sqlalchemy.TextClause()`): A class representing a SQL
+                   query.
+
+           Returns:
+               `fig` from create_graph().
+        """
+        engine = create_engine("sqlite:///plotly_dash/palmerpenguins.sq3")
+        df = pd.read_sql_query(query, engine)
+        df = df.replace(r"^\s*$", np.nan, regex=True)
+        df = df.dropna()
+
+        return self.create_graph(df)
+
+    def create_graph(self, df: pd.DataFrame()) -> go.Figure():
+        """Create a 3D scattergraph.
+           
+           Create a 3D scattergraph from the three variables and pass it and 
+           the dataframe to draw_plane().
+           
+           Params:
+               df (`pd.DataFrame()`): A dataframe with 3 columns for the 
+                   explanatory and response variables.
+
+           Returns:
+               `fig` from draw_plane().
+        """
+        fig = px.scatter_3d(df, x=self.first_explanatory, 
+                            y=self.second_explanatory, z=self.response,)
+
+        return self.fit_model(df, fig)
+    
+    def fit_model(self, df: pd.DataFrame(), fig: go.Figure()) -> go.Figure():
+        """Fit a multiple regression model.
+
+           Fit a multiple regression model to the data using least squares.
+
+           Params:
+               df (`pd.DataFrame()`): A dataframe with 3 columns for the 
+                   explanatory and response variables.
+               fig (`go.Figure()`): A Plotly Express 3D scattergraph.
+        
+           Returns:
+               `fig` from draw_plane().
+
+        """
+        first_explanatory = df[self.first_explanatory] # ! df[...] deprecated?
+        second_explanatory = df[self.second_explanatory]
+        response = df[self.response]
+        X = pd.concat([first_explanatory, second_explanatory], axis=1)
+        y = response
+        X = sm.add_constant(X)
+        model = sm.OLS(y, X).fit()
+
+        return self.draw_plane(first_explanatory, second_explanatory, model,
+                               fig)
+    
+    def draw_plane(
+            self, 
+            first_explanatory: pd.Series(),
+            second_explanatory: pd.Series(),
+            model: sm.regression.linear_model.RegressionResultsWrapper(),
+            fig: go.Figure()
+            ) -> go.Figure():
+        """Draw a plane of best fit.
+
+           From the multiple regression model, draw a plane of best fit to the
+           graph and return it. 
+
+           Params:
+               first_explanatory (`pd.Series()`): ...
+               second_explanatory (`pd.Series()`): ...
+               model (`sm.regression.linear_model.RegressionResultsWrapper()`):
+                   ...
+               fig (`go.Figure()`): A Plotly Express 3D scattergraph.
+
+           Returns:
+               `fig`, a Plotly Express 3D scattergraph (`go.Figure()`).
+        """
+        z_intercept = model.params[0]
+        x_slope = model.params[1]
+        y_slope = model.params[2]
+        xx1, xx2 = np.meshgrid(
+            np.linspace(first_explanatory.min(), first_explanatory.max()),
+            np.linspace(second_explanatory.min(), second_explanatory.max())
+            )
+        z = z_intercept + (x_slope * xx1) + (y_slope * xx2)
+
+        fig.add_trace(go.Surface(x=xx1, y=xx2, z=z))
 
         return fig
--- plotly_dash/multiple_regression/multiple_regression.py
+++ plotly_dash/multiple_regression/multiple_regression.py
@@ -3,8 +3,8 @@ import sys
 
 from dash import Dash, dcc, html, Output, Input
 
-# sys.path.append("plotly_dash/")
-# from data_viz import MultipleRegression
+sys.path.append("plotly_dash/")
+from data_viz import MultipleRegression
 
 # Build components.
 app = Dash(__name__)
@@ -19,7 +19,6 @@ species_radio = dcc.RadioItems([
     value="'Adelie%'",
     inline=True
     )
-# ! Make it so the user can't select the same value for all axes.
 first_explanatory_radio = dcc.RadioItems([
     {"label": "Culmen Length (mm)", "value": "culmen_length_mm"},
     {"label": "Culmen Depth (mm)", "value": "culmen_depth_mm"},
@@ -78,7 +77,6 @@ app.layout = html.Div([
         ], className='response-radio')
     ])
 
-# ? Explain callback? Needs type hinting?
 @app.callback(
     Output(graph, component_property='figure'),
     Input(species_radio, component_property='value'),
@@ -88,9 +86,26 @@ app.layout = html.Div([
     )
 def update_graphs(species: str, first_explanatory: str, 
                   second_explanatory: str, response: str) -> go.Figure():
-    """"""
+    """Update the graph from user inputs.
+
+       Take in the user's choice of penguin species and explanatory and 
+       response variables and return a multiple regression 3D scattergraph.
+
+       Params:
+           species (str): The user's choice of penguin species.       
+           first_explanatory (str): The user's choice for the first explanatory 
+               variable.
+           second_explanatory (str): The user's choice for the second
+               explanatory variable.
+           response (str): The user's choice for the response variable.
+
+       Returns:
+           Calls splom.__call__() and scattergraph.__call__(), which return a 
+           Plotly Express scatterplot matrix and 3D scattergraph respectively 
+           (`go.Figure()`).
+    """
     figure = MultipleRegression(species, first_explanatory, second_explanatory,
-                                response).query_sql()
+                                response).build_query()
 
     return figure
 
