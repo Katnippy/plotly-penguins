9c3220c9dc8e0f6fb581395d8bf813a775097b84
kat <kat@pop-os.localdomain>
2023-09-18 13:39:06 +0100

    Add Plotly Dash template for demonstrate multiple regression interactively.

---
+++ plotly_dash/multiple_regression/multiple_regression.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+import sys
+
+from dash import Dash, dcc, html, Output, Input
+
+# sys.path.append("plotly_dash/")
+# from data_viz import MultipleRegression
+
+# Build components.
+app = Dash(__name__)
+
+title = dcc.Markdown(children='# Multiple regression.')
+graph = dcc.Graph(figure={})
+species_radio = dcc.RadioItems([
+    {"label": "Adelie", "value": "'Adelie%'"},
+    {"label": "Chinstrap", "value": "'Chinstrap%'"},
+    {"label": "Gentoo", "value": "'Gentoo%'"}
+    ],
+    value="'Adelie%'",
+    inline=True
+    )
+# ! Make it so the user can't select the same value for all axes.
+first_explanatory_radio = dcc.RadioItems([
+    {"label": "Culmen Length (mm)", "value": "culmen_length_mm"},
+    {"label": "Culmen Depth (mm)", "value": "culmen_depth_mm"},
+    {"label": "Flipper Length (mm)", "value": "flipper_length_mm"},
+    {"label": "Body Mass (g)", "value": "body_mass_g"},
+    {"label": "δ15N (‰)", "value": "delta_15_N_ppt"},
+    {"label": "δ13C (‰)", "value": "delta_13_C_ppt"}
+    ],
+    value="body_mass_g",
+    inline=True
+    )
+second_explanatory_radio = dcc.RadioItems([
+    {"label": "Culmen Length (mm)", "value": "culmen_length_mm"},
+    {"label": "Culmen Depth (mm)", "value": "culmen_depth_mm"},
+    {"label": "Flipper Length (mm)", "value": "flipper_length_mm"},
+    {"label": "Body Mass (g)", "value": "body_mass_g"},
+    {"label": "δ15N (‰)", "value": "delta_15_N_ppt"},
+    {"label": "δ13C (‰)", "value": "delta_13_C_ppt"}
+    ],
+    value="culmen_length_mm",
+    inline=True
+    )
+response_radio = dcc.RadioItems([
+    {"label": "Culmen Length (mm)", "value": "culmen_length_mm"},
+    {"label": "Culmen Depth (mm)", "value": "culmen_depth_mm"},
+    {"label": "Flipper Length (mm)", "value": "flipper_length_mm"},
+    {"label": "Body Mass (g)", "value": "body_mass_g"},
+    {"label": "δ15N (‰)", "value": "delta_15_N_ppt"},
+    {"label": "δ13C (‰)", "value": "delta_13_C_ppt"}
+    ],
+    value="culmen_depth_mm",
+    inline=True
+    )
+
+# Customise page layout.
+app.layout = html.Div([
+    html.Div(title),
+    html.Div(graph),
+    html.Div(children=[
+        html.H4("Filter by species", style={"display": "inline"}),
+        species_radio
+        ]),
+    html.Div(children=[
+        html.H4("First explanatory variable (x-axis)", 
+                style={"display": "inline"}),
+        first_explanatory_radio
+        ], className='first-explanatory-radio'),
+    html.Div(children=[
+        html.H4("Second explanatory variable (y-axis)", 
+                style={"display": "inline"}),
+        second_explanatory_radio
+        ], className='second-explanatory-radio'),    
+    html.Div(children=[
+        html.H4("Response variable (z-axis)", style={"display": "inline"}),
+        response_radio
+        ], className='response-radio')
+    ])
+
+# ? Explain callback? Needs type hinting?
+@app.callback(
+    Output(graph, component_property='figure'),
+    Input(species_radio, component_property='value'),
+    Input(first_explanatory_radio, component_property='value'),
+    Input(second_explanatory_radio, component_property='value'),
+    Input(response_radio, component_property='value')
+    )
+def update_graphs(species: str, first_explanatory: str, 
+                  second_explanatory: str, response: str) -> go.Figure():
+    """"""
+    figure = MultipleRegression(species, first_explanatory, second_explanatory,
+                                response).query_sql()
+
+    return figure
+
+# Run app with debug enabled.
+if __name__ == "__main__":
+    app.run_server(debug=True)
